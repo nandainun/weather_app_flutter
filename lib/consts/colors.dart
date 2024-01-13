2e1309d15059a012f39168b86f7320bfc1f75623
Nanda Ilhami <1nandailhami@gmail.com>
2024-01-13 23:07:24 +0700

    starter project

---
+++ lib/consts/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color(0xFF2C3333);
+const cardColor = Color(0xFF2E4F4F);
+const textColor = Colors.white;
+const dividerColor = Color(0xFFCBE4DE);
